af0a4b9473d368b1acfbfb19aad205432d7d110d
GuiMarquesCode <guilherme.csantos@sptech.school>
2025-10-01 15:05:42 -0300

    Teste pré utilização

--- extrai-acao/src/main/java/school/sptech/Teste.java
+++ extrai-acao/src/main/java/school/sptech/Teste.java
@@ -1,18 +1,19 @@
 package school.sptech;
 
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.List;
+
+
 
 public class Teste {
     private String token = "dbESckEYvyLTKmNAE9HL3v";
     String ticker = "PETR4";
 
 
-    public String infos(String token, String ticker) {
+    public void infos(String token, String ticker) {
 
         try {
 
@@ -26,22 +27,28 @@ public class Teste {
                     .uri(URI.create(url))
                     .build();
 
-
             HttpResponse<String> response = client.send(request,
                     HttpResponse.BodyHandlers.ofString());
 
-            return response.body();
+
+            String responseBody = response.body();
+
+
+
+
+
+
 
         } catch (Exception e) {
             e.printStackTrace();
-            return null;
+            return ;
         }
     }
 
     public static void main(String[] args) {
         Teste t = new Teste();
-        String resposta = t.infos(t.token, t.ticker);
-        System.out.println(resposta);
+         t.infos(t.token, t.ticker);
+
     }
 
 }
--- extrai-acao/src/main/java/school/sptech/TratarExcel.java
+++ extrai-acao/src/main/java/school/sptech/TratarExcel.java
@@ -1,9 +1,45 @@
 package school.sptech;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class TratarExcel {
+    private String token = "dbESckEYvyLTKmNAE9HL3v";
 
     public void tratamentoDados(String nomeArquivo){
 
+
+
+    }
+
+
+
+    public String infos(String token, String ticker) {
+
+        try {
+
+            String url = "https://brapi.dev/api/quote/" + ticker
+                    + "?token=" + token
+                    + "&modules=summaryProfile";
+
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .build();
+
+
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+
+            return response.body();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
