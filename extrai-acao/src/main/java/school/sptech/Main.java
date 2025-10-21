9571c010580b500b9126c7688e212a26a4ff232a
VictorDevSPTech33 <victor.orenga@sptech.school>
2025-10-21 17:33:58 -0300

    alteração no Main.java

--- extrai-acao/src/main/java/school/sptech/Main.java
+++ extrai-acao/src/main/java/school/sptech/Main.java
@@ -6,8 +6,6 @@ public class Main {
     public static void main(String[] args) {
 
         LeitorExcel exec = new LeitorExcel();
-       exec.extrairAcoes("b3_stocks_1994_2020.xlsx");
-
-
+       exec.extrairAcoes("/home/ubuntu/proj/smart-investment-java/extrai-acao/src/main/resources/b3_stocks_1994_2020.xlsx");
     }
 }
